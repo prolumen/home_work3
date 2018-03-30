d271e617083bdeb5b3a8e81669854af800fec2a4
Andrey Pritula <a.s.pritula@icloud.com>
2018-03-30 23:01:36 +0300

    Initial commit

---
+++ src/home_work4/MyEnter.java
@@ -0,0 +1,9 @@
+package home_work4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+abstract class MyEnter {
+    Scanner scan = new Scanner(System.in);
+    Random rand = new Random();
+}
---
+++ src/home_work4/SumOfEnter.java
@@ -0,0 +1,22 @@
+package home_work4;
+
+
+public class SumOfEnter extends MyEnter{
+    SumOfEnter(){
+        System.out.println("enter the right limit:");
+        int sc = scan.nextInt();
+        System.out.print("\nres\t");
+        sum(sc);
+    }
+     void sum(int sc){
+        int res = 0;
+        for (int i = 1; i<= sc; i++){
+            res += i;
+        }
+        System.out.print(res);
+    }
+
+    public static void main(String[] args) {
+        SumOfEnter s = new SumOfEnter();
+    }
+}
