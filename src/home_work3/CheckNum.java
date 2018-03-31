3e3eedbbd3da1c5fd7c408d3f5a7d72cb2a357a7
Andrey Pritula <a.s.pritula@icloud.com>
2018-03-31 23:24:19 +0300

    New changelist

---
+++ src/home_work3/CheckNum.java
@@ -0,0 +1,43 @@
+package home_work3;
+
+public class CheckNum extends MyEnter {
+    CheckNum() {
+        System.out.println("Введите трехзначное число:");
+        int n = scan.nextInt();
+        int a, b, c;
+        c = n % 10;
+        b = ((n % 100) - c) / 10;
+        a = (n - (n % 100)) / 100;
+        System.out.println("Определить четная ли сумма цифр числа - ввести 1\nОпределить возрастание цифр в числе - введите 2\n");
+        String str = scan.next();
+        str.toString();
+
+        if (str.equals("1")){
+            if (EvenOdd(a,b,c)) {
+                System.out.println("Четное");
+            } else System.out.println("Не четное");
+        } else if (str.equals("2")){
+            if (Increase(a,b,c)) {
+                System.out.println("Возрастающее");
+            } else System.out.println("Не возрастающее");
+        } else System.out.println("Good buy!!!");
+            System.out.println("Завершение программы ");
+        }
+
+    boolean EvenOdd(int a, int b, int c){
+        int res = (a + b + c)%2;
+        if (res == 0){
+            return true;
+        } else return false;
+    }
+    boolean Increase(int a, int b, int c){
+        if ((a<b)||(b<c)){
+            return true;
+        } else return false;
+    }
+
+    public static void main(String[] args) {
+        CheckNum checkNum = new CheckNum();
+
+    }
+}
--- src/lesson/SumFuncDemo.java
+++ src/lesson/SumFuncDemo.java
@@ -3,15 +3,14 @@ package lesson;
 import java.util.Scanner;
 
 public class SumFuncDemo {
-    static void SumDemo(int k){
+    static void SumDemo(int k) {
         int a, b, c;
-        c = k%10;
-        b = ((k%100) - c)/10;
-        a = (k-(k%100))/100;
-
+        c = k % 10;
+        b = ((k % 100) - c) / 10;
+        a = (k - (k % 100)) / 100;
         System.out.println(a+" "+b+" "+c);
-
     }
+
     public static void main(String[] args) {
         System.out.println(" ");
         Scanner sc = new Scanner(System.in);
